56907794065e0f3cf1f73f9decb825f43f6c7b81
Dany DeB <de.brito.ln@gmail.com>
2022-10-11 06:49:26 +0200

    11/10 Trying parallax

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import background from '../assets/img/homepage-background.jpg'
+
 import HeaderNav from './HeaderNav'
 
 import '../styles/Header.css'
@@ -6,6 +8,7 @@ function Header() {
 
     return (
         <div className="homepage-header">
+            <img id='homepage-header-bg' src={background} alt="fond" />
             <div className="homepage-header-gradient"></div>
             <HeaderNav />
             <div className="homepage-header-content">
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,13 +2,30 @@ import Header from './Header'
 
 import '../styles/Homepage.css'
 
+function Parallax() {
+    return (
+        <script>
+            let img=getElementByid('homepage-header-bg');
+
+            window.addEventListener('scroll', function() {
+                let value = window.ScrollY
+            })
+        </script>
+    )
+}
+
 function Homepage() {
 
     return (
         <div className='homepage'>
             <Header />
+            <Parallax />
         </div>
+        
     )
 }
+
+
+
 
 export default Homepage
