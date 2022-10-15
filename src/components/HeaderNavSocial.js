e909426329ef12d9e503d94981d5ef022f0d0d4d
Dany DeB <de.brito.ln@gmail.com>
2022-10-15 21:42:48 +0200

    HeaderNavSocial restarting

--- src/components/HeaderNavSocial.js
+++ src/components/HeaderNavSocial.js
@@ -1,38 +1,46 @@
 import NetworkIcon from './NetworkIcon'
 
 import '../styles/HeaderNavSocial.css'
+import { useState } from 'react'
 
 function HeaderNavSocial() {
-    const menu = document.getElementsByClassName('.homepage-head-nav-socialnetwork');
+    const [toggle, setToggle] = useState(false)
     
     return (
         <div className="homepage-head-nav-socialnetwork">
-            <div className='homepage-head-nav-socialnetwork-button' 
-            onClick={() => (menu.classList.toggle('active'),
-            console.log("ca marche"))
-            
-            }
-        >
-                <NetworkIcon name='Share' />
+            <div 
+                className="homepage-head-nav-socialnetwork-toggle"
+                onClick={() => (
+                    toggle ? (
+                        setToggle(false),
+                        console.log('set to true')
+                    )
+                    : (
+                        console.log('set to false'),
+                        setToggle(true)
+                    )
+                )}
+            >
+                <NetworkIcon name='Share'/>
             </div>
             <li>
-                <a href="https://instagram.com" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='Instagram' />
-                </a>
-            </li>
-            <li>
-                <a href="https://www.facebook.com/" target='_blank' rel='noreferrer'>
+                <a href="https://facebook.com" target='_blank' rel='noreferrer'>
                     <NetworkIcon name='Facebook' />
                 </a>
             </li>
             <li>
-                <a href="https://www.ubereats.com" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='UberEat' />
+                <a href="https://www.instagram.com/" target='_blank' rel='noreferrer'>
+                    <NetworkIcon name='Instagram' />
                 </a>
             </li>
             <li>
-                <a href="https://www.google.fr/maps/@46.4433156,2.3644762,6.08z" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='Map'/>
+                <a href="https://www.ubereats.com/fr" target='_blank' rel='noreferrer'>
+                    <NetworkIcon name='UberEat' className='test' />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.google.com/maps/@46.701621,1.7564313,6.13z" target='_blank' rel='noreferrer'>
+                    <NetworkIcon name='Map' />
                 </a>
             </li>
         </div>
