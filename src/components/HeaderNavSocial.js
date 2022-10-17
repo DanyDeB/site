d750962ebec6a8373979245280792b6f437e6907
Dany DeB <de.brito.ln@gmail.com>
2022-10-17 08:49:11 +0200

    HeaderNavSocial done

--- src/components/HeaderNavSocial.js
+++ src/components/HeaderNavSocial.js
@@ -7,16 +7,16 @@ function HeaderNavSocial() {
     const [toggle, setToggle] = useState(false)
     
     return (
-        <div className="homepage-head-nav-socialnetwork">
+        <div className={`homepage-head-nav-socialnetwork ${toggle ? "active" : ""}`}>
             <div 
                 className="homepage-head-nav-socialnetwork-toggle"
                 onClick={() => (
                     toggle ? (
                         setToggle(false),
-                        console.log('set to true')
+                        console.log('---set to true---')
                     )
                     : (
-                        console.log('set to false'),
+                        console.log('---set to false---'),
                         setToggle(true)
                     )
                 )}
@@ -25,22 +25,22 @@ function HeaderNavSocial() {
             </div>
             <li>
                 <a href="https://facebook.com" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='Facebook' />
+                    <NetworkIcon name='Facebook' className="homepage-head-nav-socialnetwork-facebook-logo" />
                 </a>
             </li>
             <li>
                 <a href="https://www.instagram.com/" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='Instagram' />
+                    <NetworkIcon name='Instagram' className="homepage-head-nav-socialnetwork-instagram-logo" />
                 </a>
             </li>
             <li>
                 <a href="https://www.ubereats.com/fr" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='UberEat' className='test' />
+                    <NetworkIcon name='UberEat' className="homepage-head-nav-socialnetwork-ubereat-logo" />
                 </a>
             </li>
             <li>
                 <a href="https://www.google.com/maps/@46.701621,1.7564313,6.13z" target='_blank' rel='noreferrer'>
-                    <NetworkIcon name='Map' />
+                    <NetworkIcon name='Map' className="homepage-head-nav-socialnetwork-map-logo" />
                 </a>
             </li>
         </div>
